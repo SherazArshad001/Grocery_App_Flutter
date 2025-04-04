b02744c94b0734e316be650f0daa7d84809af9c0
Sheraz Baloch <itsbalouch001@gmail.com>
2025-04-05 00:26:33 +0500

    OnBoarding Controller Code Updated

--- lib/controller/onboarding_controller.dart
+++ lib/controller/onboarding_controller.dart
@@ -3,16 +3,33 @@ import 'package:get/get.dart';
 
 class OnboardingController extends GetxController {
   var currentPage = 0.obs;
-  final pageController = PageController();
+  final PageController pageController = PageController();
+
+  @override
+  void onClose() {
+    pageController.dispose(); // Dispose of the PageController
+    super.onClose();
+  }
 
   void nextPage() {
     if (currentPage.value < 2) {
+      currentPage.value++; // Increment currentPage value
       pageController.nextPage(
         duration: const Duration(milliseconds: 500),
         curve: Curves.ease,
       );
     } else {
       Get.offAllNamed('/home'); // Navigate to Home after last onboarding screen
+    }
+  }
+
+  void previousPage() {
+    if (currentPage.value > 0) {
+      currentPage.value--; // Decrement currentPage value
+      pageController.previousPage(
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.ease,
+      );
     }
   }
 
