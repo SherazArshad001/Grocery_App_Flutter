97ae327182330b5d400145db4e72e619f5f9c2e5
Sheraz Baloch <itsbalouch001@gmail.com>
2025-04-05 00:16:08 +0500

    App Routes Class Code Updated

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,23 +1,45 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
 import 'package:grocery_app/views/home_screen.dart';
 import 'package:grocery_app/views/onboarding.dart';
 import 'package:grocery_app/views/product_details_screen.dart';
 import 'package:grocery_app/views/splash_screen.dart';
 
 class AppRoutes {
+  // Route names for easier navigation and to avoid magic strings
   static const String splash = '/splash';
   static const String boarding = '/boarding';
-
   static const String home = '/home';
   static const String productDetails = '/productDetails';
 
+  // List of all defined routes with corresponding pages
   static List<GetPage> pages = [
-    GetPage(name: splash, page: () => const SplashScreen()),
-    GetPage(name: boarding, page: () => const OnboardingScreen()),
-    GetPage(name: home, page: () => HomeScreen()),
+    // Splash Screen Route
+    GetPage(
+      name: splash,
+      page: () => const SplashScreen(),
+      transition: Transition.fadeIn, // Optional transition
+    ),
+
+    // Onboarding Screen Route
+    GetPage(
+      name: boarding,
+      page: () => const OnboardingScreen(),
+      transition: Transition.rightToLeft, // Optional transition
+    ),
+
+    // Home Screen Route
+    GetPage(
+      name: home,
+      page: () => HomeScreen(),
+      transition: Transition.fadeIn, // Optional transition
+    ),
+
+    // Product Details Route
     GetPage(
       name: productDetails,
-      page: () => ProductDetailsScreen(),
+      page: () => const ProductDetailsScreen(),
+      transition: Transition.rightToLeftWithFade, // Optional transition
     ),
   ];
 }
