fb5a476007fa9f607078ef9ecf5890c6db579a04
Sheraz Baloch <itsbalouch001@gmail.com>
2025-04-05 00:21:11 +0500

    Category Model Code Updated

--- lib/models/cetagory_model.dart
+++ lib/models/cetagory_model.dart
@@ -6,4 +6,18 @@ class CategoryModel {
     required this.name,
     required this.imageUrl,
   });
+
+  // Equality operator to compare instances of CategoryModel
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is CategoryModel &&
+        other.name == name &&
+        other.imageUrl == imageUrl;
+  }
+
+  // HashCode override to ensure equality comparisons are consistent
+  @override
+  int get hashCode => name.hashCode ^ imageUrl.hashCode;
+
 }
