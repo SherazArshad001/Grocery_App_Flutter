b5a648a9a946af73ed5fc4d313d079316e6b6534
Sheraz Baloch <itsbalouch001@gmail.com>
2025-04-05 00:10:01 +0500

    Splash Screen Code Updated

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:grocery_app/controller/splash_controller.dart';
 import 'package:grocery_app/utils/app_assets.dart';
 import 'package:grocery_app/utils/app_constant.dart';
 import 'package:grocery_app/utils/app_style.dart';
@@ -10,8 +8,6 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Get.put(SplashController());
-
     return Scaffold(
       body: Stack(
         children: [
