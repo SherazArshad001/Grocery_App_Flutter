2e1c1bc1f7544900e06a935e77edaf92059f9128
Sheraz Baloch <itsbalouch001@gmail.com>
2025-04-05 00:22:38 +0500

    Splash Controller Code Updated

--- lib/controller/splash_controller.dart
+++ lib/controller/splash_controller.dart
@@ -1,19 +1,34 @@
 import 'package:get/get.dart';
-import '../routes/app_routes.dart';
+import 'package:grocery_app/routes/app_routes.dart';
 
 class SplashController extends GetxController {
+  // Flag to track if the navigation was already triggered
+  bool isNavigated = false;
+
   @override
   void onInit() {
     super.onInit();
     _navigateToOnBoarding();
   }
 
+  // Method to handle the navigation after a delay
   void _navigateToOnBoarding() {
+    if (isNavigated) return; // Prevent multiple navigation triggers
+
+    // Delay navigation
     Future.delayed(
       const Duration(seconds: 3),
       () {
-        Get.offNamed(AppRoutes.boarding);
+        if (!isNavigated) {
+          isNavigated = true;
+          Get.offNamed(AppRoutes.boarding);
+        }
       },
     );
+  }
+
+  // Optional: Method to cancel the navigation if needed (e.g., user navigates away)
+  void cancelNavigation() {
+    isNavigated = true;
   }
 }
