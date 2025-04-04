a75041f51b83d559277151b2fb253b1cb068fcc6
Sheraz Baloch <itsbalouch001@gmail.com>
2025-04-05 00:13:53 +0500

    App Constant Class Code Updated

--- lib/utils/app_constant.dart
+++ lib/utils/app_constant.dart
@@ -1,22 +1,24 @@
 class AppConstants {
-  // App Name
+  // App Info
   static const String appName = "Grocery App";
+
+  // Splash Screen Text
+  static const String splashText = "Welcome to\nBig Cart";
+
+  // Onboarding Screen Text
+  static const String onBodingText1 = "Buy Premium\nQuality Fruits";
+  static const String onBodingText2 = "Buy Quality\nDairy Products";
+  static const String onBodingText3 = "Get Discounts\nOn All Products";
 
   // Button Text
   static const String getStarted = "Get Start";
   static const String addToCart = "Add To Cart";
   static const String next = "Next";
   static const String search = "Search";
+
+  // Categories Text
   static const String categories = "Categories";
+
+  // Featured Product Section Text
   static const String featureProduct = "Feature Product";
-
-
-
-  
-
-  static const String splashText = "Welcome to\nBig Cart";
-
-  static const String onBodingText1 = "Buy Premium\nQuality Fruits";
-  static const String onBodingText2 = "Buy Quality\nDairy Products";
-  static const String onBodingText3 = "Get Discounts\nOn All Products";
 }
