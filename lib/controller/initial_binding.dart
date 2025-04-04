ed48d10a086869174470770e489bcfd03946c317
Sheraz Baloch <itsbalouch001@gmail.com>
2025-04-05 00:29:36 +0500

    Updated Code

--- lib/controller/initial_binding.dart
+++ lib/controller/initial_binding.dart
@@ -1,11 +1,15 @@
 import 'package:get/get.dart';
 import 'package:grocery_app/controller/splash_controller.dart';
 import 'package:grocery_app/controller/product_controller.dart';
+import 'package:grocery_app/controller/onboarding_controller.dart'; // Example of service
 
 class InitialBinding extends Bindings {
   @override
   void dependencies() {
+    // Lazily initialize SplashController and ProductController
     Get.lazyPut(() => SplashController());
-    Get.lazyPut(() => ProductController()); // Add ProductController here
+    Get.lazyPut(() => ProductController());
+    Get.lazyPut(() => OnboardingController());
+    // Example of adding OnboardingController
   }
 }
